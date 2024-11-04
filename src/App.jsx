bbd17dfc8fdce688e62677ddebdac9c882f73da2
Jessica Moore <jessica.cmoore1@gmail.com>
2024-11-04 13:57:58 -0500

    :bento: Add static categories (will add images later)

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { AVAILABLE_CATEGORIES } from '@/constants/categories.js';
+
 function App() {
   const [count, setCount] = useState(0)
 
---
+++ src/constants/categories.js
@@ -0,0 +1,58 @@
+export const AVAILABLE_CATEGORIES = [
+  {
+    id: 9,
+    name: 'General Knowledge',
+  },
+  {
+    id: 10,
+    name: 'Books',
+  },
+  {
+    id: 11,
+    name: 'Film',
+  },
+  {
+    id: 12,
+    name: 'Music',
+  },
+  {
+    id: 14,
+    name: 'Television',
+  },
+  {
+    id: 15,
+    name: 'Video Games',
+  },
+  {
+    id: 17,
+    name: 'Science & Nature',
+  },
+  {
+    id: 21,
+    name: 'Sports',
+  },
+  {
+    id: 22,
+    name: 'Geography',
+  },
+  {
+    id: 23,
+    name: 'History',
+  },
+  {
+    id: 24,
+    name: 'Politics',
+  },
+  {
+    id: 25,
+    name: 'Art',
+  },
+  {
+    id: 26,
+    name: 'Celebrities',
+  },
+  {
+    id: 27,
+    name: 'Animals',
+  }
+];
