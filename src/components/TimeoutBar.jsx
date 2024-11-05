e3e09b696f77a369248f70dc7604a9bdb36b4a4d
Jessica Moore <jessica.cmoore1@gmail.com>
2024-11-05 14:04:59 -0500

    :lipstick: Dynamically style progress bar base on answer state

--- src/components/TimeoutBar.jsx
+++ src/components/TimeoutBar.jsx
@@ -1,4 +1,34 @@
 import { useState, useEffect } from 'react';
+import styled from 'styled-components';
+
+const StyledProgress = styled.progress`
+  border-radius: 0.5rem;
+  width: 50%;
+  height: 1rem;
+
+  ${({ $mode }) => {
+    const barColor = $mode === 'answered' ? '#FAD9BD' 
+                  : $mode === 'correct' ? '#D7EED7' 
+                  : $mode === 'wrong' ? '#eb8c95' 
+                  : '#99DBF8';
+                  
+    const valueColor = $mode === 'answered' ? '#f39f5a' 
+                   : $mode === 'correct' ? '#8dcd8d' 
+                   : $mode === 'wrong' ? '#dc3545' 
+                   : '#00a6ed';
+
+    return `
+      &::-webkit-progress-bar {
+        border-radius: 0.5rem;
+        background-color: ${barColor};
+      }
+      &::-webkit-progress-value {
+        border-radius: 0.5rem;
+        background-color: ${valueColor};
+      }
+    `;
+  }}
+`;
 
 function TimeoutBar({ timeout, onTimeout, mode }) {
   const [remainingTime, setRemainingTime] = useState(timeout);
@@ -19,7 +49,7 @@ function TimeoutBar({ timeout, onTimeout, mode }) {
     };
   }, [timeout, onTimeout]);
 
-  return <progress id="timeout-bar" className={mode} max={timeout} value={remainingTime} />;
+  return <StyledProgress $mode={mode} id="timeout-bar" className={mode} max={timeout} value={remainingTime} />;
 }
 
 export default TimeoutBar;
